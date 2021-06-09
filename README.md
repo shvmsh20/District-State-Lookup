6314e87cf37052131bc209d278cd9ee1496e1286
shvmsh20 <itsshivamsharma21@gmail.com>
2021-06-09 19:35:51 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# District State Lookup
+![Application image](images/application-img.png)
+
+An application build using HTML, CSS and JavaScript. It can be used to find in which state or union territory of India the searched district belongs. It shows all the districts that match the current input. 
---
+++ js/main.js
@@ -0,0 +1,47 @@
+const search = document.querySelector("#search");
+const matchList = document.querySelector("#match-list");
+
+//Search states.json and filter
+
+const searchStates =  searchText =>{
+
+         fetch("../data/states.json")
+        .then( res => res.json())
+        .then( data => {
+
+            const arr_objs = data.states;
+            let output="";
+            arr_objs.forEach( (obj) =>{
+                const regex = new RegExp(`^${searchText}`,"gi");
+                const districts = obj.districts;
+                const matching_dists = districts.filter( (district) =>{
+                    return district.match(regex);
+                })
+
+                if(matching_dists.length>0){
+                    matching_dists.forEach( (district)=>{
+                        output+=
+                        `
+                        <div class="card">
+                        <h2>${district} <span class="stateName">${obj.state}</span></h2>
+                        </div>
+                        `
+                    })
+                }
+                
+            })
+            //Show result in HTML
+            if(searchText==""){
+                matching_dists=[];
+                output="";
+            }
+            matchList.innerHTML = output;
+            
+        })
+    };
+
+
+
+search.addEventListener("input",function(){
+    searchStates(search.value);
+})
